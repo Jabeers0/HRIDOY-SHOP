3a419862e7ad56756f37940f43574d09c5e2220e
HRIDOY <147415922+Jabeers0@users.noreply.github.com>
2025-07-11 11:22:00 +0600

    Create index.js

---
+++ login/index.js
@@ -0,0 +1,6 @@
+document.querySelectorAll('.buy-now').forEach(btn => {
+  btn.addEventListener('click', () => {
+    alert(`Feature in progress: Buy Nitro plan`);
+    // Replace this or integrate actual payment logic
+  });
+});
